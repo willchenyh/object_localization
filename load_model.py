d375bda41b54630147c742025bb36f15130b5c61
Will Chen <willchenyh@gmail.com>
2017-12-20 23:01:56 -0800

    remove a fc layer

--- load_model.py
+++ load_model.py
@@ -47,7 +47,7 @@ def load_xception():
     print('Model weights loaded.')
     base_out = base_model.output
     x = GlobalAveragePooling2D()(base_out)
-    x = Dense(2048, activation='relu')(x)
+    # x = Dense(2048, activation='relu')(x)
     x = Dense(2048, activation='relu')(x)
     predictions = Dense(NUM_COORDS, activation='sigmoid')(x)
     model = Model(inputs=base_model.input, outputs=predictions)
